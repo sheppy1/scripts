f297c415f21a7b0e7c9e969b884b431bd58ad0f5
sheppy1 <Dayle.shepherd@gmail.com>
2024-03-19 15:15:02 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "backup" {
+  type = object({
+    type                 = string
+    tier                 = optional(string)
+    interval_in_minutes  = optional(number)
+    interval_in_hours    = optional(number)
+    storage_redundancy   = optional(string)
+  })
+
+  validation {
+    condition = (
+      var.backup.type == "Continuous" || var.backup.type == "Periodic"
+    )
+    error_message = "Invalid value for backup.type. Possible values are Continuous and Periodic."
+  }
+
+  validation {
+    condition     = (
+      var.backup.tier == null || var.backup.tier == "Continuous7Days" || var.backup.tier == "Continuous30Days"
+    )
+    error_message = "Invalid value for backup.tier. Possible values are Continuous7Days and Continuous30Days."
+  }
+
+  validation {
+    condition     = (
+      var.backup.interval_in_minutes == null || var.backup.interval_in_minutes >= 60 && var.backup.interval_in_minutes <= 1440
+    )
+    error_message = "Invalid value for backup.interval_in_minutes. Must be between 60 and 1440."
+  }
+
+  validation {
+    condition     = (
+      var.backup.interval_in_hours == null || var.backup.interval_in_hours >= 8 && var.backup.interval_in_hours <= 720
+    )
+    error_message = "Invalid value for backup.interval_in_hours. Must be between 8 and 720."
+  }
+
+  validation {
+    condition     = (
+      var.backup.storage_redundancy == null || var.backup.storage_redundancy == "Geo" || var.backup.storage_redundancy == "Local" || var.backup.storage_redundancy == "Zone"
+    )
+    error_message = "Invalid value for backup.storage_redundancy. Possible values are Geo, Local, and Zone."
+  }
+}
