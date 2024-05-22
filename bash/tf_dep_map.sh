d01d595a7aca229a2202e6076495945a44157977
sheppy1 <Dayle.shepherd@gmail.com>
2024-05-22 14:50:19 +0100

    Update tf_dep_map.sh

--- bash/tf_dep_map.sh
+++ bash/tf_dep_map.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Function to find all *.tf files recursively, excluding 'integration_test' directories
 find_tf_files() {
     find "$1" -type d -name 'integration_test' -prune -o -type f -name "*.tf" -print
 }
 
-# Function to parse module dependencies in a *.tf file
 parse_tf_file() {
     local file="$1"
     local current_module
     current_module=$(basename "$(dirname "$file")")
-    grep -oP 'module\s+"\K[^"]+' "$file" | while read -r module; do
-        echo "$current_module -> $module"
+    grep -oP '(module|data)\s+"\K[^"]+' "$file" | while read -r resource_type module_name; do
+        echo "$current_module -> $module_name ($resource_type)"
     done
 }
 
-# Main function to create the dependency map for all modules in the given directory
 create_dependency_map() {
     local directory="$1"
     declare -A module_dependencies
@@ -23,8 +20,9 @@ create_dependency_map() {
     while IFS= read -r tf_file; do
         while IFS= read -r dependency; do
             module=$(echo "$dependency" | awk '{print $1}')
-            dependent_module=$(echo "$dependency" | awk '{print $3}')
-            module_dependencies["$module"]+="$dependent_module "
+            dependent=$(echo "$dependency" | awk '{print $3}')
+            type=$(echo "$dependency" | awk '{print $5}')
+            module_dependencies["$module"]+="$dependent ($type) "
         done < <(parse_tf_file "$tf_file")
     done < <(find_tf_files "$directory")
 
@@ -34,7 +32,6 @@ create_dependency_map() {
     done
 }
 
-# Directory containing the Terraform configurations
-terraform_directory="./terraform_configs"  # Change this to your Terraform directory
+terraform_directory="./terraform_configs" 
 
 create_dependency_map "$terraform_directory"
